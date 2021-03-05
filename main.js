d7451cc812156e5bd04bd0187dd1881a8440fb2f
anastasiaptakh <sayeslife@mail.ru>
2021-03-05 17:46:57 +0300

    solve js chaining task

--- main.js
+++ main.js
@@ -174,3 +174,47 @@ let calculator = {
 console.log(calculator.read())
 console.log(calculator.sum())
 console.log(calculator.mul())
+
+/* 7
+ Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // показывает текущую ступеньку
+    alert( this.step );
+  }
+};
+Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+
+ladder.up().up().down().showStep(); // 1
+*/
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() { // показывает текущую ступеньку
+    console.log(this.step);
+    return this;
+  }
+};
+
+console.log(ladder.up().up().down().showStep()); 
+
