a6c40b70a1410e5714ffe6701679aadf524bdeb3
anastasiaptakh <sayeslife@mail.ru>
2021-03-16 17:11:26 +0300

    solve number methods task

--- main.js
+++ main.js
@@ -248,10 +248,10 @@ console.log(changeWord('5o5'));
 */
 
 
-function readNumber(){
+function readNumber() {
   let num;
   do {
-    num = prompt('введите число' , 0)
+    //num = prompt('введите число' , 0)
   } while (typeof num === 'number')
 
   if(num === null || num === '') return null;
@@ -259,3 +259,22 @@ function readNumber(){
 }
 
 console.log(readNumber());
+
+/*
+Встроенный метод Math.random() возвращает случайное число от 0 (включительно)
+ до 1 (но не включая 1)
+
+Напишите функцию random(min, max), которая генерирует 
+случайное число с плавающей точкой от min до max (но не включая max).
+
+Пример работы функции:
+
+alert( random(1, 5) ); // 1.2345623452
+alert( random(1, 5) ); // 3.7894332423
+alert( random(1, 5) ); // 4.3435234525
+*/
+
+function random(min, max) {
+  return (Math.random() * (max-min) + min) 
+}
+console.log(random(1, 5))
