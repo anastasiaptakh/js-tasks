d5e2a885a47c7005364441f4a57056bbe41fa524
anastasiaptakh <sayeslife@mail.ru>
2021-03-17 11:45:56 +0300

    solve task using string methods

--- main.js
+++ main.js
@@ -299,3 +299,29 @@ console.log(checkSpam('buy ViAgRA now'));
 console.log(checkSpam('free xxxxx'));
 console.log(checkSpam("innocent rabbit"));
 
+/*
+Создайте функцию truncate(str, maxlength),
+ которая проверяет длину строки str и, если она 
+ превосходит maxlength, заменяет конец str на "…", 
+ так, чтобы её длина стала равна maxlength.
+
+Результатом функции должна быть та же строка, если усечение 
+не требуется, либо, если необходимо, усечённая строка.
+
+Например:
+
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+
+truncate("Всем привет!", 20) = "Всем привет!"
+*/
+
+function truncate(str , maxlength) {
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + '...';
+  } else {
+     return str;
+  }
+}
+console.log(truncate("Всем привет!", 20));
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+
